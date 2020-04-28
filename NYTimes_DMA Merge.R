f6ad775b24a930f2e618060053c6a369e63ba242
paysola <64175100+paysola@users.noreply.github.com>
2020-04-27 21:31:36 -0400

    Update NYTimes_DMA Merge.R

--- NYTimes_DMA Merge.R
+++ NYTimes_DMA Merge.R
@@ -11,7 +11,6 @@ nytimes_county_state <- unite(nytimes,
 nytimes_county_state$Identifier <- NULL
 nytimes_county_state$X <- NULL
 head(nytimes_county_state)
-str(nytimes_county_state)
 ##Clean DMA
 dma <- read.csv("county_dma.csv")
 dma$CNTYFP <- NULL
@@ -20,10 +19,18 @@ dma$DMAINDEX <- NULL
 dma$STATEFP <- NULL
 dma$County <- trimws(dma$COUNTY, which = c("both"))
 dma$COUNTY <-NULL
+###Remove spaces from DMA **May be different from Aarons data
 dma$DMA <- trimws(dma$DMA, which = c("both"))
 head(dma, 1)
-dma_v1 <- unite(dma, County_State, c(County,STATE), sep = ",", remove=TRUE)
-head(dma_v1)
-str(dma_v1)
-merged <- left_join(nytimes_county_state, dma_v1, by = c("County_State" = "County_State"))
-merged
+dma_1 <- unite(dma, County_State, c(County,STATE), sep = ",", remove=TRUE)
+head(dma_1)
+##To fix error message with merge
+dma_1$County_State<- trimws(dma_1$County_State, which =c("both"))
+head(dma_1, 1)
+merged <- left_join(nytimes_county_state, dma_1, by = c("County_State" = "County_State"))
+head(merged)
+##add new column Identifier (date_DMA)
+nytimes_dma<- unite(merged, date_DMA, c(date,DMA), sep = "_", remove=FALSE)
+head(nytimes_dma, 5)
+##save file
+write.csv(nytimes_dma,'nytimes_dma.csv')
