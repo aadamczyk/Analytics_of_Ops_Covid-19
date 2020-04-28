7757d72c68aa5d55e5993d8d0cb18a1ca0e8e406
Alex Adamczyk <alex.john.adamczyk@gmail.com>
2020-04-28 19:06:39 -0400

    cleanACS function
    Update the cleanACS function to update the column names automatically from the metadate

--- Clean_ACS.R
+++ Clean_ACS.R
@@ -1,28 +1,37 @@
 library(tidyverse)
 
+data <- "../Raw Data/ACSST5Y2018.S0101_data_with_overlays_2020-04-26T231254.csv"
+metadata <- "../Raw Data/Metadata/ACSST5Y2018.S0101_metadata_2020-04-26T231254.csv"
+
+cleanACS <- function(data, metadata){
+    df <- read_csv(data, skip = 1)
+    dfNames <- read_csv(data, n_max = 1)
+    dfNames <- colnames(dfNames)
+
+    meta <- read_csv(metadata, col_names = F)
+
+    meta[3][is.na(meta[3])] <- 0
+
+    meta %>%
+        filter(X3 == 1) %>%
+        select(X1) -> desiredColumns
+
+    df <- df[,dfNames %in% desiredColumns$X1]
+    dfNames <- dfNames[dfNames %in% desiredColumns$X1]
+    dfNames <- as.data.frame(dfNames, stringsAsFactors = F)
+    dfNames <- left_join(dfNames, meta, by = c("dfNames" = "X1"))
+
+    colnames(df) <- dfNames$X4
+
+    return(df)
+}
+
 # Age ---------------------------------------------------------------------
 
-age <- read_csv("../Raw Data/ACSST5Y2018.S0101_data_with_overlays_2020-04-26T231254.csv",
-                       skip = 1)
-agenames <- read_csv("../Raw Data/ACSST5Y2018.S0101_data_with_overlays_2020-04-26T231254.csv",
-                     n_max = 1)
-agenames <- colnames(agenames)
+age <- "../Raw Data/ACSST5Y2018.S0101_data_with_overlays_2020-04-26T231254.csv"
+ageMeta <- "../Raw Data/Metadata/ACSST5Y2018.S0101_metadata_2020-04-26T231254.csv"
 
-ageMeta <- read_csv("../Raw Data/Metadata/ACSST5Y2018.S0101_metadata_2020-04-26T231254.csv",
-                    col_names = F)
-
-ageMeta[3][is.na(ageMeta[3])] <- 0
-
-ageMeta %>%
-    filter(X3 == 1) %>%
-    select(X1) -> desiredColumns
-
-age <- age[,agenames %in% desiredColumns$X1]
-agenames <- agenames[agenames %in% desiredColumns$X1]
-agenames <- as.data.frame(agenames, stringsAsFactors = F)
-agenames <- left_join(agenames, ageMeta, by = c("agenames" = "X1"))
-
-colnames(age) <- agenames$X4
+age <- cleanACS(data = age, metadata = ageMeta)
 
 acs <- age
 
