be414168bba85832b57166d5c386a47ce417fd90
Alex Adamczyk <alex.john.adamczyk@gmail.com>
2020-05-03 14:29:15 -0400

    Remove lag and lead loops for V1
    Remove the lagging and leading loops for the base case of the model.

--- Run Analyses.R
+++ Run Analyses.R
@@ -28,146 +28,145 @@ model_data <- select(final_data, -c(X, location, geo_code, Inflation.Factor, DMA
 lag = 1
 lead = 14
 
-for (lead in 1:14)
-{
-  for (lag in 1:14)
-  {
+#for (lead in 1:14)
+#{
+#  for (lag in 1:14)
+#  {
     covid <-    model_data %>% drop_na(paste("CaseLagDay.",lag,sep=""))
     covid <-    covid %>% drop_na(paste("CaseLeadDay",lead,sep=""))
-    
+
     for (i in 1:14)
     {
       covid <- select(covid, -c(paste("DeathLeadDay",i,sep="")))
-      
+
       if (i != lead){
         covid <- select(covid, -c(paste("CaseLeadDay",i, sep="")))
       }
-      
+
       if (i > lag){
         covid <- select(covid, -c(paste("CaseLagDay.",i, sep=""),paste("DeathLagDay.",i,sep="")))
       }
-      
+
     }
-    
+
     set.seed(144)
     split = createDataPartition(covid$CaseLeadDay1, p = 0.75, list = FALSE)
     covid.train <- covid[split,]
     covid.test <- covid[-split,]
-    
+
     #Train Linear Model
-    
+
     model <- lm(CaseLeadDay14 ~ . - date,
                 data = covid.train)
-    
+
     summary(model)
-    
+
     pred.covid <- predict(model, newdata = covid.test)
-    
+
     SSE.improved = sum((pred.covid - covid.test$CaseLeadDay14)^2)
     SST.original = sum((mean(covid.train$CaseLeadDay14) - covid.test$CaseLeadDay14)^2)
     OSR2.improved = 1 - (SSE.improved/SST.original)
     OSR2.improved
-    
+
     #Train LASSO
-    
+
     #1. Ridge Regression & LASSO Train
-    
+
     set.seed(15071)
-    
+
     #1.1. Preparation of the train and test matrices
-    
-    x.train=model.matrix(CaseLeadDay14~. - date, data=covid.train) 
+
+    x.train=model.matrix(CaseLeadDay14~. - date, data=covid.train)
     y.train= covid.train$CaseLeadDay14
-    x.test=model.matrix(CaseLeadDay14~. - date, data=covid.test) 
+    x.test=model.matrix(CaseLeadDay14~. - date, data=covid.test)
     y.test=covid.test$CaseLeadDay14
-    
+
     lambdas.lasso <- exp(seq(15, -10, -.01))
-    
+
     #1.2 Cross Validate
-    
+
     ridge.tr <- cv.glmnet(x.train, y.train, lambda = lambdas.lasso, nfolds = 10, alpha=0)
     lasso.tr <- cv.glmnet(x.train, y.train, lambda = lambdas.lasso, nfolds = 10, alpha=1)
-    
+
     plot(ridge.tr)
     plot(lasso.tr)
-    
+
     ridge.tr$lambda.min
     lasso.tr$lambda.min
-    @
-      
-      #2. Develop Model
-      ridge.fin <- glmnet(x.train, y.train, alpha = 0, lambda = ridge.tr$lambda.min)
+
+    #2. Develop Model
+    ridge.fin <- glmnet(x.train, y.train, alpha = 0, lambda = ridge.tr$lambda.min)
     lasso.fin <- glmnet(x.train, y.train, alpha = 1, lambda = lasso.tr$lambda.min)
-    
+
     #2. Show Coefficients
     coef(ridge.fin)
     coef(lasso.fin)
-    
+
     #3. R^2 & OSR^2
-    
+
     #3.1 Prediction
     pred.ridge.train <- predict(ridge.fin, newx = x.train)
     pred.lasso.train <- predict(lasso.fin, newx = x.train)
-    
+
     pred.ridge.test <- predict(ridge.fin, newx = x.test)
     pred.lasso.test <- predict(lasso.fin, newx = x.test)
-    
+
     #3.2 Residuals
-    ridge.train.resid <- covid.train$CaseLeadDay14 - pred.ridge.train 
-    lasso.train.resid <- covid.train$CaseLeadDay14 - pred.lasso.train 
-    
-    ridge.test.resid <- covid.test$CaseLeadDay14 - pred.ridge.test 
-    lasso.test.resid <- covid.test$CaseLeadDay14 - pred.lasso.test 
-    
+    ridge.train.resid <- covid.train$CaseLeadDay14 - pred.ridge.train
+    lasso.train.resid <- covid.train$CaseLeadDay14 - pred.lasso.train
+
+    ridge.test.resid <- covid.test$CaseLeadDay14 - pred.ridge.test
+    lasso.test.resid <- covid.test$CaseLeadDay14 - pred.lasso.test
+
     #3.3 R^2 & OSR^2
-    
+
     R2.ridge <- 1 - sum(ridge.train.resid^2) / sum((covid.train$CaseLeadDay14 - mean(covid.train$CaseLeadDay14))^2)
     R2.lasso <- 1 - sum(lasso.train.resid^2) / sum((covid.train$CaseLeadDay14 - mean(covid.train$CaseLeadDay14))^2)
-    
+
     OSR2.ridge <- 1 - sum(ridge.test.resid^2) / sum((covid.test$CaseLeadDay14 - mean(covid.train$CaseLeadDay14))^2)
     OSR2.lasso <- 1 - sum(lasso.test.resid^2) / sum((covid.test$CaseLeadDay14 - mean(covid.train$CaseLeadDay14))^2)
-    
+
     R2.ridge
     OSR2.ridge
-    
+
     R2.lasso
     OSR2.lasso
-    
+
     #xGBoost
-    
+
     xgb <- train(y = y.train,
                  x = x.train,
                  method = "xgbTree",
                  trControl = trainControl(method="cv", number=5))
-    
+
     xgb$bestTune
-    
-    
+
+
     # R^2 and OSR^2
     xgb.final <- xgb$finalModel
-    
+
     preds.xgb.train <- predict(xgb.final, newdata = x.train)
     preds.xgb.test <- predict(xgb.final, newdata = x.test)
-    
-    
+
+
     r2.xgb <- 1 - sum((preds.xgb.train - y.train)^2)/sum((mean(y.train) - y.train)^2)
     osr.xgb <- 1 - sum((preds.xgb.test - y.test)^2)/sum((mean(y.train) - y.test)^2)
-    
+
     r2.xgb
     osr.xgb
-    
+
     #Feature Importance
     mat <- xgb.importance(colnames(covid.train), model =xgb.final)
     xgb.plot.importance(mat)
-    
+
     nodes <- xgb.dump(xgb.final, with_stats = T)
     nodes[1:10]
-    
+
     covid.test.review <- select(covid.test, c("CaseLeadDay14", "date", "hits", "hits.adj", "New_Cases", "New_Deaths", "Total_Cases", "Total_Deaths"))
-    
+
     covid.test.review$Predict <- preds.xgb.test
-    
+
     covid.test.review$Predict[covid.test.review$Predict < 0] <- 0
-    
-  }
-}
+
+#  }
+#}
