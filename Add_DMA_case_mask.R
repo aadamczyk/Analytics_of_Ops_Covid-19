6faa5d83f8a3041bcc127e56ae8a20f7b11110ac
Alex Adamczyk <alex.john.adamczyk@gmail.com>
2020-11-28 00:51:25 -0500

    Weighted Average of NYT Data
    Had forgotten to aggregate the NYT data, had multiple records for each DMA for each day (one per county).
    Added in the county population from the ACS then took the weighted average of the survey data from each county.

--- Add_DMA_case_mask.R
+++ Add_DMA_case_mask.R
@@ -1,5 +1,6 @@
 library(readr)
 library(dplyr)
+library(stringr)
 
 nyt <- read_csv("cleaned_case_mask_data.csv")
 
@@ -7,8 +8,6 @@ nyt <- read_csv("cleaned_case_mask_data.csv")
 nyt$county[nyt$county == "Anchorage"] <- "Anchorage Borough"
 nyt$county[nyt$county == "Juneau City and Borough"] <- "Juneau Borough"
 nyt$county[nyt$county == "Miami-Dade"] <- "Dade"
-nyt$county[nyt$county == "Baltimore city"] <- "Baltimore City"
-nyt$county[nyt$county == "St. Louis city"] <- "St. Louis City"
 nyt$county[nyt$county == "Doña Ana"] <- "Dona Ana"
 # For some reason Broomfield County is missing, but it's clearly in the Denver DMA
 nyt$county[nyt$county == "Broomfield"] <- "Denver"
@@ -43,8 +42,44 @@ county_dma <- read_csv("county_dma2.csv")
 
 nyt <- left_join(nyt, county_dma, by = c("state" = "STATE", "county" = "COUNTY"))
 
+# Add populations and aggregate
+acs <- read_csv("ACS.csv")
+acs %>%
+    select(`Geographic Area Name`, TotPop) %>%
+    mutate(geo = str_remove_all(`Geographic Area Name`, " County"),
+           geo = str_remove_all(geo, " Parish")) %>%
+    select(-`Geographic Area Name`) -> acs
+
+acs$geo[acs$geo == "Anchorage Municipality, Alaska"] <- "Anchorage Borough, Alaska"
+acs$geo[acs$geo == "Juneau City and Borough, Alaska"] <- "Juneau Borough, Alaska"
+acs$geo[acs$geo == "Miami-Dade, Florida"] <- "Dade, Florida"
+acs$geo[acs$geo == "Do<U+FFFD>a Ana, New Mexico"] <- "Dona Ana, New Mexico"
+
 nyt %>%
-    select(date, DMA, cases, deaths, new_cases, new_deaths, NEVER, RARELY,
+    mutate(geo = paste0(county, ", ", state)) -> nyt
+
+nyt <- left_join(nyt, acs, by = c("geo" = "geo"))
+
+nyt %>%
+    select(date, DMA, TotPop, cases, deaths, new_cases, new_deaths, NEVER, RARELY,
            SOMETIMES, FREQUENTLY, ALWAYS) -> nyt
+
+nyt %>%
+    group_by(date, DMA) %>%
+    mutate(DMA_pop = sum(TotPop), pop_share = TotPop / DMA_pop) %>%
+    mutate(NEVER = NEVER * pop_share,
+           RARELY = RARELY * pop_share,
+           SOMETIMES = SOMETIMES * pop_share,
+           FREQUENTLY = FREQUENTLY * pop_share,
+           ALWAYS = ALWAYS * pop_share) %>%
+    summarise(cases = sum(cases),
+              deaths = sum(deaths),
+              new_cases = sum(new_cases),
+              new_deaths = sum(new_deaths),
+              NEVER = sum(NEVER),
+              RARELY = sum(RARELY),
+              SOMETIMES = sum(SOMETIMES),
+              FREQUENTLY = sum(FREQUENTLY),
+              ALWAYS = sum(ALWAYS)) -> nyt
 
 write_csv(nyt, "Data/cleaned_case_mask_DMAs.csv")
--- Join_Files.R
+++ Join_Files.R
@@ -1,20 +1,29 @@
 library(readr)
 library(stringr)
+library(dplyr)
+library(lubridate)
 
 acs_dma <- read_csv("acs_dma_final.csv")
 
-nyt_dma <- read_csv("cleaned_case_mask_data.csv")
-
-nyt_dma %>%
-    select(-X1) %>%
-    mutate(DMA = str_sub(string = Category,
-                         start = str_locate(Category, "_")[,1]+1)) -> nyt_dma
+nyt_dma <- read_csv("Data/cleaned_case_mask_DMAs.csv")
 
 nyt_acs_dma <- left_join(nyt_dma, acs_dma, by = "DMA")
 
-colSums(is.na(nyt_acs_dma))
+mobility <- read_csv("Data/mobility_output/mobility_agg.csv")
 
-nyt_acs_dma %>%
-    filter(is.na(TotPop_sum)) -> missingACS
+mobility %>%
+    mutate(date = ymd(str_sub(id, start = 1, end = 8)),
+           DMA = str_sub(id, start = 10)) %>%
+    select(-X1) -> mobility
+
+nyt_acs_mob <- left_join(nyt_acs_dma, mobility,
+                         by = c("date" = "date", "DMA" = "DMA"))
+
+missing <- nyt_acs_mob[!complete.cases(nyt_acs_mob),]
+
+missing %>%
+    select(date, DMA, id) -> missing
+
+nyt_acs_mob <-
 
 write_csv(nyt_acs_dma, "nyt_acs_dma.csv")
